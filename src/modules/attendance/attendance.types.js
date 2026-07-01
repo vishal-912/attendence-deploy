bbdb6ddc0714ba5df2500d72092b0ecfbd62a0b9
divesh sharma <divesh01111@gmail.com>
2026-07-01 20:59:27 +0530

    updated types.js

---
+++ src/modules/attendance/attendance.types.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const AttendanceStatus = Object.freeze({
+  PRESENT: 'PRESENT',
+  ABSENT: 'ABSENT',
+  LATE: 'LATE',
+  EXCUSED: 'EXCUSED',
+  LEAVE: 'LEAVE',
+});
+
+module.exports = {
+  AttendanceStatus,
+};
